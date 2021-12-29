fc4458040c7e0a0ebb7a90e8c4e6a78ca179a763
niveditha999 <52778443+niveditha999@users.noreply.github.com>
2021-12-29 13:56:28 +0530

    Update addindustry.php
    Committed

--- addindustry.php
+++ addindustry.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" type="text/css" href="addindustry.css">
          <style>
         body{
-            background-image: url(images/iimage2.jpg);
+            background-image: url(iimage2.jpg);
             background-size: cover;
             
             
