2005d2f28cd41668fb85fe28058766e697ebe98b
niveditha999 <52778443+niveditha999@users.noreply.github.com>
2021-12-29 19:22:34 +0530

    Create admin.php
    First Commit

---
+++ admin.php
@@ -0,0 +1,54 @@
+
+<html>
+    <head>
+        <title>admin login</title>
+        <link rel="stylesheet" type="text/css" href="admin.css">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <style>
+            body{
+                background-image: url(login1.jpg);
+            }
+        </style>
+        
+    </head>
+    <body>
+        <div id="main">
+         
+        <nav>
+            <a id="pk"></a>
+
+            <ul>
+                
+               <li><a href="main.html">HOME</a></li> 
+                <li><a href="gallery.html">GALLERY</a></li>  
+            </ul>
+        </nav>
+        </div>
+        <form method="post" >
+        <div class="admin">
+            <img src="avatar.png" class="avatar">
+            <h1>Admin Login</h1>
+            <form>
+            <p>Username</p>
+            <input type="text" name="uname" placeholder="Enter Username">
+            <p>Password</p>
+            <input type="password" name="pass" placeholder="Enter Password">
+               <input type="submit" name="submit" value="Login">
+            
+            <a href="#">Forget Password!!</a>
+            </form>
+        </div>
+       
+            </form>
+        
+         <?php
+        if(isset($_POST['uname']) and isset($_POST['pass'])){
+        if($_POST['uname']=='admin'and $_POST['pass']=='admin'){
+           header('location:adminview.php');
+        }}
+        ?>
+
+       
+
+    </body>
+</html>
