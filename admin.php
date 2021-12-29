3f9f8d6ec1c409b2ee64246bd34556cf28f7b489
niveditha999 <52778443+niveditha999@users.noreply.github.com>
2021-12-29 19:39:08 +0530

    Update admin.php
    commit change

--- admin.php
+++ admin.php
@@ -3,7 +3,7 @@
          <?php
         if(isset($_POST['uname']) and isset($_POST['pass'])){
         if($_POST['uname']=='admin'and $_POST['pass']=='admin'){
-           header('location:adminview.php');
+           header('location:adminview.html');
         }}
         ?>
 
