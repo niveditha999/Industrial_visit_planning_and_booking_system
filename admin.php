7b02537ed61de294656ad98d0e429c04ad666b11
niveditha999 <52778443+niveditha999@users.noreply.github.com>
2021-12-29 19:42:13 +0530

    Update and rename admin.html to admin.php
    Comit change

--- admin.php
+++ admin.php
@@ -42,7 +42,17 @@
             </form>
         
          <?php
-       include("admin.php")
+       
+        
+         <?php
+        if(isset($_POST['uname']) and isset($_POST['pass'])){
+        if($_POST['uname']=='admin'and $_POST['pass']=='admin'){
+           header('location:adminview.html');
+        }}
+        ?>
+
+      
+
         ?>
 
        
